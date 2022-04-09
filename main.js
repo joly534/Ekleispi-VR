57ca53a7bf0eb4e5a74a5297e52a7c524f7c2e93
Cthulhu <joly534@gmail.com>
2022-04-09 16:16:56 +0200

    update
    add animation on desk

--- main.js
+++ main.js
@@ -16,12 +16,12 @@ setInterval(() => {
 }, 500);
 
 
-let video= document.getElementById('video_prestations');
-video.setAttribute('src', 'assets/video/prestations.mp4');
-video.setAttribute('width', '3');
-video.setAttribute('height', '1.5');
-video.setAttribute('position', '-7.46 2.25 -9.99');
-video.setAttribute('loop', 'false');
+// let video= document.getElementById('video_prestations');
+// video.setAttribute('src', 'assets/video/prestations.mp4');
+// video.setAttribute('width', '3');
+// video.setAttribute('height', '1.5');
+// video.setAttribute('position', '-7.46 2.25 -9.99');
+// video.setAttribute('loop', 'false');
 
 let ring= document.getElementById('ring');
 let camera = document.getElementById('camera');
@@ -40,4 +40,29 @@ document.addEventListener('keydown', function (e) {
         camera.object3D.position.z +=0.05;
 
     } 
+});
+
+
+// éléments du bureau
+let bureau = [];
+
+let tiroir1 = document.getElementById('tiroir1');
+let tiroir2 = document.getElementById('tiroir2');
+let tiroir3 = document.getElementById('tiroir3');
+let tiroir4 = document.getElementById('tiroir4');
+let tiroir5 = document.getElementById('tiroir5');
+let tiroir6 = document.getElementById('tiroir6');
+
+bureau.push(tiroir1, tiroir2, tiroir3, tiroir4, tiroir5, tiroir6);
+
+bureau.forEach(element => {
+    element.addEventListener('mouseenter', function(){
+        element.setAttribute('position', {z: 0.6}, true);
+
+    })
+    element.addEventListener('mouseleave', function(){
+        element.setAttribute('position', {z: 0.214}, true);
+
+    })
+    
 });
