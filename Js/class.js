4b83e86cb330c92f548b657bfa813200086b558f
Cthulhu <joly534@gmail.com>
2022-04-11 17:57:08 +0200

    update
    change the office

---
+++ Js/class.js
@@ -0,0 +1,16 @@
+class Sphere {
+    constructor(scene){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.radius = 0.2;
+        this.color = white;
+
+        this.draw = function() {
+            let light  = scene.createElement('a-sphere');
+            light.setAttribute('radius', this.radius);
+            light.setAttribute('')
+            
+        }
+    }
+}
